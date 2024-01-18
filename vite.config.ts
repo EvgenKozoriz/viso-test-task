4fb2df869ad38d62fadc25300d41fb04da341d5d
Eugene Kozoriz <zkozoriz11@gmail.com>
2024-01-18 10:25:45 +0200

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    VITE_APP_GOOGLE_MAPS_API_KEY: JSON.stringify(
+      process.env.VITE_APP_GOOGLE_MAPS_API_KEY
+    ),
+
+    VITE_APP_GOOGLE_MAPS_MAP_ID: JSON.stringify(
+      process.env.VITE_APP_GOOGLE_MAPS_MAP_ID
+    ),
+
+    VITE_APP_FIREBASE_API_KEY: JSON.stringify(
+      process.env.VITE_APP_FIREBASE_API_KEY
+    ),
+  },
 });
